32161c80f7d30eca201bba8c3c0348762b3b1a0a
antoniofloresrdz-ugto <antonio.flores.rdz@gmail.com>
2025-10-05 17:29:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,6 @@ async function loadData(){
     const page = document.body.getAttribute('data-page');
     if(page === 'home') populateHome(data);
     if(page === 'product') populateProduct(data);
-
-    
-    const htmlToInsert = `
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T5ZLNV3V');</script>
-<!-- End Google Tag Manager -->
-`;
-
-document.head.insertAdjacentHTML('beforeend', htmlToInsert);
 }
 
 function populateHeaderFooter(store){
